21c38f4cde9d5f3fe887939a8c265ea86374a25e
Chia2035 <kp.chia9666@gmail.com>
2022-03-04 22:16:29 +0800

    Formatted to different style

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
-let playGame = confirm('Want to play Rock, Paper, Scissor?');
+let playGame = confirm("Want to play Rock, Paper, Scissor?");
 
 //Initialize Gmae
 if (playGame) {
-    //Give player choice
-    let playerChoice = prompt('Pick rock, paper or scissor');
+  //Give player choice
+  let playerChoice = prompt("Pick rock, paper or scissor");
 
-    //Valid choice?
-    if (playerChoice) {
-        let playerOne = playerChoice.trim().toLowerCase;
-        if (playerOne === 'rock' || playerOne === 'paper' || playerOne === 'scissor') {
-            let computerChoice = Math.floor(Math.random() * 3 + 1);
-            let computer = computerChoice === 1 ? 'rock' 
-                          :computerChoice === 2 ? 'paper' 
-                                                : 'scissor';
+  //Valid choice?
+  if (playerChoice) {
+    let playerOne = playerChoice.trim().toLowerCase();
+    if (
+      playerOne === "rock" ||
+      playerOne === "paper" ||
+      playerOne === "scissor"
+    ) {
+      //Generate computer choice
+      let computerChoice = Math.floor(Math.random() * 3 + 1);
+      let computer =
+        computerChoice === 1
+          ? "rock"
+          : computerChoice === 2
+          ? "paper"
+          : "scissor";
+      //Compare Results
+      let result =
+        playerOne === computer
+          ? "Tie Game!"
+          : playerOne === "rock" && computer !== "scissor"
+          ? "You lost!"
+          : playerOne === "paper" && computer !== "rock"
+          ? "You lost!"
+          : playerOne === "scissor" && computer !== "paper"
+          ? "You lost!"
+          : "You won!";
+      alert(result);
 
-            let result = 
-            playerOne === computer ? 'Tie Game!'
-            : playerOne === 'rock' && computer !== 'scissor' ? 'You lost!'
-            : playerOne === 'paper' && computer !== 'rock' ? 'You lost!'
-            : playerOne === 'scissor' && computer !== 'paper' ? 'You lost!'
-            : 'You won!';
-            alert(result);
-            
-            confirm("Want to play again?") ? location.reload() : 'See you next time!'
-        } else {
-            alert("Not a valid choice.")
-        }
+      confirm("Want to play again?") ? location.reload() : "See you next time!";
     } else {
-        alert('Oh... it\'s alright D:');
+      alert("Not a valid choice.");
+      location.reload();
     }
+  } else {
+    alert("Oh... it's alright D:");
+  }
 } else {
-    alert('Alright, maybe next time.');
+  alert("Alright, maybe next time.");
 }
