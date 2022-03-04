0a25dc2fd720973e5041d31001b938d4c4c6dcf2
Chia2035 <kp.chia9666@gmail.com>
2022-03-04 17:37:31 +0800

    playerOne choice and computerChoice

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ let playGame = confirm('Want to play Rock, Paper, Scissor?');
 //Initialize Gmae
 if (playGame) {
     //Give player choice
-    let playerChoice = prompt('Pick rock, paper or scissor')
-    if () {
+    let playerChoice = prompt('Pick rock, paper or scissor');
 
+    //Valid choice?
+    if (playerChoice) {
+        let playerOne = playerChoice.trim().toLowerCase;
+        if (playerOne === 'rock' || playerOne === 'paper' || playerOne === 'scissor') {
+            let computerChoice = Math.floor(Math.random()*3 + 1);
+            let computer = computerChoice === 1 ? 'rock'
+            : computerChoice === 2 ? 'paper'
+            : computerChoice === 3 ? 'scissor'
+        }
     } else {
-        alert('Oh, it\'s alright.');
+        alert('Oh... it\'s alright D:');
     }
 } else {
-    alert('Alright, maybe next time');
+    alert('Alright, maybe next time.');
 }
