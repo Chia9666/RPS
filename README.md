b1bf0a227c907c6fb6265c45dc11a0dc9e398506
Chia9666 <kp.chia9666@gmail.com>
2022-03-12 14:24:16 +0800

    Finished web response RPS game

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # RPS
-Simple Rock Paper Scissor Game
 First Simple Game Project
+Responsive Web Rock Paper Scissor Game
+Shows html css and javascript DOM manipulation skills.
--- main.js
+++ main.js
@@ -1,5 +1,115 @@
-let playGame = confirm("Want to play Rock, Paper, Scissor?");
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const fire_div = document.getElementById("fire");
+const water_div = document.getElementById("water");
+const grass_div = document.getElementById("grass");
 
+function getComputerChoice() {
+  let choices = new Map();
+  choices.set(0, "f").set(1, "w").set(2, "g");
+  /* const choices = ["f", "w", "g"];*/
+  let randomNumber = Math.floor(Math.random() * 3);
+  return choices.get(randomNumber);
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  const fullName = new Map();
+  fullName.set("f", "Fire").set("w", "Water").set("g", "Grass");
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${fullName.get(
+    userChoice
+  )}(User) superEffective against ${fullName.get(
+    computerChoice
+  )}(Computer).<br> You Win!`;
+
+  document
+    .getElementById(fullName.get(userChoice).toLowerCase())
+    .classList.add("green-glow");
+  setTimeout(function () {
+    document
+      .getElementById(fullName.get(userChoice).toLowerCase())
+      .classList.remove("green-glow");
+  }, 350);
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  const fullName = new Map();
+  fullName.set("f", "Fire").set("w", "Water").set("g", "Grass");
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${fullName.get(
+    computerChoice
+  )}(Computer) superEffective against ${fullName.get(
+    userChoice
+  )}(User).<br> You Lose!`;
+  document
+    .getElementById(fullName.get(userChoice).toLowerCase())
+    .classList.add("red-glow");
+  setTimeout(function () {
+    document
+      .getElementById(fullName.get(userChoice).toLowerCase())
+      .classList.remove("red-glow");
+  }, 350);
+}
+
+function draw(userChoice, computerChoice) {
+  const fullName = new Map();
+  fullName.set("f", "Fire").set("w", "Water").set("g", "Grass");
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `Both are the same type.<br> It's a draw!`;
+  document
+    .getElementById(fullName.get(userChoice).toLowerCase())
+    .classList.add("grey-glow");
+  setTimeout(function () {
+    document
+      .getElementById(fullName.get(userChoice).toLowerCase())
+      .classList.remove("grey-glow");
+  }, 350);
+}
+
+function game(userChoice) {
+  let computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "fg":
+    case "wf":
+    case "gw":
+      win(userChoice, computerChoice);
+      break;
+    case "fw":
+    case "wg":
+    case "gf":
+      lose(userChoice, computerChoice);
+      break;
+    case "ff":
+    case "ww":
+    case "gg":
+      draw(userChoice, computerChoice);
+  }
+}
+
+function main() {
+  fire_div.addEventListener("click", function () {
+    game("f");
+  });
+  water_div.addEventListener("click", function () {
+    game("w");
+  });
+  grass_div.addEventListener("click", function () {
+    game("g");
+  });
+}
+
+main();
+/* choices_div.forEach((choice) => choice.addEventListener("click", main));
+let playGame = confirm("Want to play Rock, Paper, Scissor?");
 //Initialize Gmae
 if (playGame) {
   //Give player choice
@@ -45,3 +155,4 @@ if (playGame) {
 } else {
   alert("Alright, maybe next time.");
 }
+*/
